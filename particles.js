547947efcaa4250b1b7a13f3bb633a7361661168
aanya nagwani <84452790+aanyanagwani@users.noreply.github.com>
2021-08-17 19:02:03 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,15 @@
+class particles {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+   this.color = color(random(0,255),random(0,255),random(0,255))
+   World.add(world,this.color);
+    }
+}
+display()
+{
+    
+}
